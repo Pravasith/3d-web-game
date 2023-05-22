5f607b86e644f50b3e2b35be2c492d10661e89f1
prav <pravasith@gmail.com>
2023-05-22 17:22:07 -0400

    feat: added game checks

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import "./style.css"
 import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
-import { GlobalGUI } from "./utils/gui"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
-// import { snowFall } from "./snow"
 
-// const gltfLoader = new GLTFLoader()
+import INIT from './initialization'
 
+const {
+    scene, camera, renderer, clock,
+} = INIT
 
-// Scene
-export const scene = new THREE.Scene()
-
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600,
-}
 
 // Models
 // gltfLoader.load(
@@ -26,96 +17,7 @@ const sizes = {
 //     }
 // )
 
-// GUI
-// const lightsGUI = new GlobalGUI({
-//     width: 400,
-// })
-
-// const { params } = lightsGUI
-
-// lightsGUI.addParam("intensity", 7)
-// lightsGUI.addParam("x", -3.79)
-// lightsGUI.addParam("y", 5)
-// lightsGUI.addParam("z", 0.65)
-// lightsGUI
-//     .add(params, "intensity")
-//     .name("Light Intensity")
-//     .min(0)
-//     .max(10)
-//     .step(0.001)
-//     .onFinishChange(() => {
-//         directionalLight.intensity = params.intensity
-//     })
-// lightsGUI
-//     .add(params, "x")
-//     .name("Light X")
-//     .min(-5)
-//     .max(5)
-//     .step(0.01)
-//     .onFinishChange(() => {
-//         directionalLight.position.x = params.x
-//     })
-// lightsGUI
-//     .add(params, "y")
-//     .name("Light Y")
-//     .min(-5)
-//     .max(5)
-//     .step(0.01)
-//     .onFinishChange(() => {
-//         directionalLight.position.y = params.y
-//     })
-// lightsGUI
-//     .add(params, "z")
-//     .name("Light Z")
-//     .min(-5)
-//     .max(5)
-//     .step(0.01)
-//     .onFinishChange(() => {
-//         directionalLight.position.z = params.z
-//     })
-
-// Lights
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
-// const directionalLight = new THREE.DirectionalLight("#ffffff", params.intensity)
-// directionalLight.position.set(params.x, params.y, params.z)
-// scene.add(directionalLight, ambientLight)
-
-
-// Camera
-const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.5,
-    800
-)
-camera.position.z = 1
-scene.add(camera)
-
-// Canvas
-const canvas = document.querySelector("canvas.webgl")
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas,
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
-renderer.physicallyCorrectLights = true
-
-// const points = snowFall(scene)
-
-// const sphere = new THREE.Mesh(
-//     new THREE.SphereGeometry(1, 32, 32),
-//     new THREE.MeshStandardMaterial()
-// )
-// scene.add(sphere)
-
-
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
-// const material = new THREE.MeshBasicMaterial()
 const material = new THREE.ShaderMaterial({
     vertexShader: `
         varying vec2 vUv;
@@ -154,27 +56,27 @@ const material = new THREE.ShaderMaterial({
     `
 })
 
-
 const plane = new THREE.Mesh(
     geometry, material
 )
 
 scene.add(plane)
 
-// Clock
-export const clock = new THREE.Clock()
+let rafID
 
 // Animations
 const tick = () => {
     // Get elapsed Time
     const elapsedTime = clock.getElapsedTime()
-    // points.position.y = -elapsedTime 
 
-    // Render again
     renderer.render(scene, camera)
-
-    // Request the next frame and call tick in it
-    requestAnimationFrame(tick)
+    rafID = requestAnimationFrame(tick)
 }
 
 tick()
+
+const stopAnimating = false
+
+stopAnimating && cancelAnimationFrame(rafID)
+
+
---
+++ src/initialization/index.js
@@ -0,0 +1,43 @@
+
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
+// Scene
+const scene = new THREE.Scene()
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600,
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.5,
+    800
+)
+camera.position.z = 1
+scene.add(camera)
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl")
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+renderer.physicallyCorrectLights = true
+
+// Clock
+const clock = new THREE.Clock()
+
+const INIT = { scene, camera, renderer, clock }
+
+export default INIT
