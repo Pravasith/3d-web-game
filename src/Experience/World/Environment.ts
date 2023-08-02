57e03060403ed82746f5c2a3ce009bc2e57574c8
prav <pravasith@gmail.com>
2023-08-02 17:43:39 -0400

    fix: addedlights

--- src/Experience/World/Environment.ts
+++ src/Experience/World/Environment.ts
@@ -1,3 +1,23 @@
+import * as THREE from 'three'
+import { Experience } from '../Experience'
+
 export default class Environment {
-    constructor() {}
+    scene: THREE.Scene
+    sunLight: THREE.DirectionalLight
+
+    constructor() {
+        this.scene = Experience.scene
+        this.setSunLight()
+    }
+
+    setSunLight() {
+        this.sunLight = new THREE.DirectionalLight('#ffffff', 4)
+        this.sunLight.castShadow = true
+        this.sunLight.shadow.camera.far = 15
+        this.sunLight.shadow.mapSize.set(1024, 1024)
+        this.sunLight.shadow.normalBias = 0.05
+        this.sunLight.position.set(3.5, 2, -1.25)
+
+        this.scene.add(this.sunLight)
+    }
 }
